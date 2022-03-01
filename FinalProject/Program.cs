859236097e75dc4235795704ca510007d6fca4c5
Samip Suwal <samipdevsuwal@gmail.com>
2022-02-28 19:25:25 -0800

    Wrote all the logic for comparing cards

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 using System.Net.Security;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
 
 
 namespace Cards
@@ -12,7 +15,6 @@ namespace Cards
         private List<Card> cards;
         private const int numberOfCards = 52;
         private Face face;
-
         public Deck()
         {
             //initialize 52 cards
@@ -27,11 +29,8 @@ namespace Cards
         private void initializeDeck()
         {
             cards = new List<Card>();
-
             Card card;
-
             //initialize faces
-
             Face[] faces = new Face[]
             {
                 Face.two, Face.three, Face.four, Face.five, Face.six, Face.seven, Face.eight, Face.nine, Face.ten, Face.Jester, Face.Queen, Face.King, Face.Ace
@@ -42,27 +41,17 @@ namespace Cards
                 Suit.club, Suit.diamond, Suit.hearts, Suit.spades
             };
 
-
-
-
             for (int faceCounter = 0; faceCounter < 13; faceCounter++)
             {
-
                 for (int suitCounter = 0; suitCounter < 4; suitCounter++)
                 {
-
                     card = new Card(faces[faceCounter], suits[suitCounter]);
                     cards.Add(card);
                 }
-
-
             }
         }
-
-
-
     }
-    class Card
+    class Card:IComparable
     {
         public Face face { get; set; }
         public Suit suit { get; set; }
@@ -71,6 +60,15 @@ namespace Cards
         {
             this.face = face;
             this.suit = suit;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+            return this.face - (obj as Card).face;
         }
 
         public override string ToString()
@@ -254,7 +252,141 @@ namespace Cards
                 return 1;
             }
             GroupOfCards groupOfCards = obj as GroupOfCards;
+
+            //if the rankings are different then we can simply send the diff
+            if (this.ranking != groupOfCards.ranking)
+            {
+                return this.ranking - groupOfCards.ranking;
+            }
+
+            //get max cards, this works for all except Pairs and flush
+            //if triplets we just need to find which one is highest
+            if (this.ranking == Ranking.Triplets)
+            {
+                return this.card1.face - groupOfCards.card1.face;
+            }
+
+            //for straight flush, straight and flush we need max card
+
+            //if straight flush, find the largest card and match
+            if (this.ranking == Ranking.StraightFlush || this.ranking == Ranking.Straight)
+            {
+                Card firstGroupMaxCard = getMaxCard(this);
+                Card secondGroupMaxCard = getMaxCard(groupOfCards);
+                return firstGroupMaxCard.CompareTo(secondGroupMaxCard);
+            }
+            //if its pairs or highcards we might need to check each card
+            if (this.ranking == Ranking.HighCard)
+            {
+                //check highest card first
+                if(getMaxCard(this).CompareTo(getMaxCard(groupOfCards)) != 0)
+                {
+                    //if the max cards don't match
+                    return getMaxCard(this).CompareTo(getMaxCard(groupOfCards));
+
+                }
+
+                if (getMidCard(this).CompareTo(getMidCard(groupOfCards)) != 0)
+                {
+                    //if the max cards don't match
+                    return getMidCard(this).CompareTo(getMidCard(groupOfCards));
+
+                }
+                
+                return getMinCard(this).CompareTo(getMinCard(groupOfCards));
+            }
+            //pairs
+            if (this.ranking == Ranking.Pair)
+            {
+                //get the card that is a pair
+                List<Card> list1 = getPairAndLonerFace(this);
+                List<Card> list2 = getPairAndLonerFace(groupOfCards);
+
+                if (list1.ElementAt(0).CompareTo(list2.ElementAt(0)) != 0)
+                {
+                    return list1.ElementAt(0).CompareTo(list2.ElementAt(0));
+                }
+                return list1.ElementAt(1).CompareTo(list2.ElementAt(1));
+            }
+
             return this.ranking - groupOfCards.ranking;
+        }
+
+        /**
+         * Return a List of card where 0 is the one of the pairs, and last index is loner card
+         */
+        public List<Card> getPairAndLonerFace(GroupOfCards groupOfCards)
+        {
+            List<Card> list = new List<Card>();
+
+            if (groupOfCards.card1.CompareTo(groupOfCards.card2.face) ==0)
+            {
+                list.Add(groupOfCards.card1);
+                list.Add(groupOfCards.card3);
+
+            }
+
+            if (groupOfCards.card1.CompareTo(groupOfCards.card3.face) == 0)
+            {
+                list.Add(groupOfCards.card1);
+                list.Add(groupOfCards.card2);
+
+            }
+            if (groupOfCards.card2.CompareTo(groupOfCards.card3.face) == 0)
+            {
+                list.Add(groupOfCards.card2);
+                list.Add(groupOfCards.card1);
+
+            }
+
+
+            return list;
+
+        }
+
+        public Card getMaxCard(GroupOfCards groupOfCards)
+        {
+            Card maxCard = groupOfCards.card1;
+            if (maxCard.CompareTo(groupOfCards.card2)<0)
+            {
+                maxCard = groupOfCards.card2;
+            }
+
+            if (maxCard.CompareTo(groupOfCards.card3)< 0)
+            {
+                maxCard = groupOfCards.card3;
+            }
+
+            return maxCard;
+        }
+
+        public Card getMinCard(GroupOfCards groupOfCards)
+        {
+            Card minCard = groupOfCards.card1;
+            if (minCard.CompareTo(groupOfCards.card2)> 0)
+            {
+                minCard = groupOfCards.card2;
+            }
+
+            if (minCard.CompareTo(groupOfCards.card3) > 0)
+            {
+                minCard = groupOfCards.card3;
+            }
+
+            return minCard;
+        }
+
+        public Card getMidCard(GroupOfCards groupOfCards)
+        {
+            if (groupOfCards.card1.CompareTo(getMaxCard(groupOfCards)) != 0 && groupOfCards.card1 != getMinCard(groupOfCards))
+            {
+                return groupOfCards.card1;
+            }
+            if (groupOfCards.card2.CompareTo(getMaxCard(groupOfCards)) != 0 && groupOfCards.card2 != getMinCard(groupOfCards))
+            {
+                return groupOfCards.card2;
+            }
+            return groupOfCards.card3;
         }
     }
 
@@ -280,7 +412,7 @@ namespace Cards
                 deckA.getCards().RemoveAt(i+2);
                 GroupOfCards tempGroupOfCards = new GroupOfCards(card1, card2, card3);
 
-                listOfGroupOfCards.add(tempGroupOfCards);
+                listOfGroupOfCards.Add(tempGroupOfCards);
             }
 
 
